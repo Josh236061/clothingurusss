8136ddb6e6b0a005f742d974a2b379e70131e786
Jessica Medina <lic.jessica.medina@gmail.com>
2022-11-23 16:37:19 -0600

    cleaned up code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user_routes');
 const clothing_product_routes= require('./clothing_product-routes');
-const postRoutes = require('./post-routes')
 
 router.use('/User', userRoutes);
 router.use('/clothing_product', clothing_product_routes);
-router.use('/posts', postRoutes)
 
 module.exports = router;
